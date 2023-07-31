66f969b40ca6b5f02aec359cd05c26f2f86bb59b
John Olson <jholson1030@gmail.com>
2023-07-31 05:09:32 -0500

    Write functions:
    defaultGrid(), makeRows(), and makeColumns(). Be sure to declare the main function at the end of the code

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName('gridRow');
 let cells = document.querySelectorAll('cell');
+
+// Makes the 16 x 16 grid
+
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Take the rows and column input and creates a grid
+
+function makeRows(rowNum) {
+    // Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = 'gridRow';
+    }
+}
+
+// Creates the columns
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        }
+    }
+}
