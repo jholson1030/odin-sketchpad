f166d14ab0c1c7ad13ba39ea09ede0a12a748ce4
John Olson <jholson1030@gmail.com>
2023-07-30 21:20:21 -0500

    Declare variables for container, rows and cells

---
+++ script.js
@@ -0,0 +1,5 @@
+// Declaring variables for the container, rows and the individual cells
+
+const container = document.getElementById('container');
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.querySelectorAll('cell');
